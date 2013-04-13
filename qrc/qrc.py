b44ea130d21e93503a436cfb32825cc897ec955f
Jeong YunWon <jeong@youknowone.org>
2013-04-13 23:58:33 +0900

    boost!

--- qrc/qrc.py
+++ qrc/qrc.py
@@ -1,23 +1,80 @@
 
 from math import sqrt, ceil, trunc
 
-def is_palindrome(num):
-    snum = str(num)
-    lsnum = len(snum)
-    for i in xrange(0, lsnum / 2):
-        if snum[i] != snum[lsnum - i - 1]:
+if True:
+    from paldic import *
+    def is_palindrome(num):
+        if num in trueset:
+            return True
+        return False
+else:
+    trueset = set([])
+    falseset = set([])
+    def is_palindrome(num):
+        if num in falseset:
             return False
-    return True
+        if num in trueset:
+            return True
+        snum = str(num)
+        lsnum = len(snum)
+        for i in xrange(0, lsnum / 2):
+            if snum[i] != snum[lsnum - i - 1]:
+                falseset.add(num)
+                return False
+        trueset.add(num)
+        return True
 
+def gen_palindrome1(snum):
+    lnum = len(snum)
+    if lnum == 1:
+        return snum
+    return snum + snum[lnum-2::-1]
+
+def gen_palindrome2(snum):
+    return snum + snum[::-1]
+
+def get_seed1(snum):
+    lnum = len(snum)
+    tlen = lnum / 2 + lnum % 2
+    return '1' + '0' * (tlen-1)
+    return snum[:tlen]
+
+def get_seed2(snum):
+    lnum = len(snum)
+    tlen = lnum / 2 + lnum % 2
+    return '9' * tlen
+
+def fairsquare_slow(start, end):
+    qstart = int(ceil(sqrt(start)))
+    qend = trunc(sqrt(end))
+    #print start, end, '------------'
+    c = 0
+    for rt in xrange(qstart, qend + 1):
+        if is_palindrome(rt) and is_palindrome(rt ** 2):
+            c += 1
+            #print rt, rt**2
+    return c
 
 def fairsquare(start, end):
     qstart = int(ceil(sqrt(start)))
     qend = trunc(sqrt(end))
 
+    sstart = get_seed1(str(qstart))
+    send = get_seed2(str(qend))
     c = 0
-    for rt in xrange(qstart, qend + 1):
-       if is_palindrome(rt) and is_palindrome(rt ** 2):
-            c += 1
+    #print start, end, '------------'
+    for seed in xrange(int(sstart), int(send) + 1):
+        sseed = str(seed)
+        pal = int(gen_palindrome1(sseed))
+        if qstart <= pal <= qend:
+            if is_palindrome(pal ** 2):
+                #print pal, pal ** 2
+                c += 1
+        pal = int(gen_palindrome2(sseed))
+        if qstart <= pal <= qend:
+            if is_palindrome(pal ** 2):
+                #print pal, pal ** 2
+                c += 1
     return c
 
 if __name__ == '__main__':
@@ -26,3 +83,4 @@ if __name__ == '__main__':
         start, end = map(int, raw_input().split(' '))
         count = fairsquare(start, end)
         print 'Case #%d:' % x, count
+
--- qrc/test_qrc.py
+++ qrc/test_qrc.py
@@ -13,10 +13,34 @@ from qrc import *
 def test_palindrome(n, r):
     assert is_palindrome(n) == r
 
+@pytest.mark.parametrize(('n', 'p1', 'p2'), {
+    (1, 1, 11),
+    (12, 121, 1221),
+    (50, 505, 5005),
+})
+def test_gen_palindrome(n, p1, p2):
+    assert gen_palindrome1(str(n)) == str(p1)
+    assert gen_palindrome2(str(n)) == str(p2)
+
 @pytest.mark.parametrize(('start', 'end', 'c'), [
     (1, 4, 2),
     (10, 120, 0),
     (100, 1000, 2),
+    (21, 584, 2),
+    (21, 484, 2),
 ])
 def test_fairsquare(start, end, c):
+    assert fairsquare_slow(start, end) == c
     assert fairsquare(start, end) == c
+
+@pytest.mark.parametrize(('n', 's1', 's2'), [
+    (1, 1, 9),
+    (11, 1, 9),
+    (12, 1, 9),
+    (100, 10, 99),
+    (1234, 10, 99),
+])
+def test_seed(n, s1, s2):
+    assert get_seed1(str(n)) == str(s1)
+    assert get_seed2(str(n)) == str(s2)
+
