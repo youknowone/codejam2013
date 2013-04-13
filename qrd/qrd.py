9102f4c901da82aaa41abb75da73c70b92c41d64
Jeong YunWon <jeong@youknowone.org>
2013-04-14 07:07:45 +0900

    sort

--- qrd/qrd.py
+++ qrd/qrd.py
@@ -61,6 +61,7 @@ class World(object):
                     self.allkeys[key] += 1
                 except KeyError:
                     self.allkeys[key] = 1
+        self.cleanup()
 
     def cleanup(self):
         chestkeys = self.chestdic.keys()
@@ -71,16 +72,16 @@ class World(object):
             for key in chest.keys:
                 if key not in chestkeys:
                     chest.keys.remove(key)
-        #self.keys.sort(key=lambda key: self.chestdic.keys().index(key))
+        self.keys.sort()
 
     def __repr__(self):
         r = '<World(\n'
         r += '  keys: {}\n'.format(self.keys)
-        r += '  chests: {\n'
+        r += '  chests(): {\n'
         for key, chests in self.chestdic.iteritems():
-            r += '    {}: [\n'.format(key)
+            r += '    {}({}): [\n'.format(key, len(chests))
             for chest in chests:
-                r += '      {},\n'.format(chest.keys)
+                r += '      #{} {},\n'.format(chest.num, chest.keys)
             r += '    ],\n'
         r += '})>'
         return r
@@ -96,13 +97,23 @@ class World(object):
 
 
 def solve(world):
-    for ckey, chests in world.chestdic.iteritems():
-        if len(chests) > world.allkeys[ckey]:
+    clone = world.clone()
+    for ckey, chests in clone.chestdic.iteritems():
+        if len(chests) > clone.allkeys[ckey]:
             return False
-    return solve_(world, [])
+    return solve_(clone, [])[0]
 
 def solve_(world, depth):
     def depprint(*ts):
+        return
+        indent = '    ' * len(depth)
+        print indent,
+        for ti in ts:
+            t = str(ti)
+            t = t.replace('\n', '\n ' + indent)
+            print t,
+        print ''
+    def okprint(*ts):
         return
         indent = '    ' * len(depth)
         print indent,
@@ -116,10 +127,13 @@ def solve_(world, depth):
     depprint('depth:', depth)
     depprint(world)
     if len(world.chestdic) == 0:
-        return []
-    for key, wchests in world.chestdic.iteritems():
-        if key not in world.keys:
+        okprint([])
+        okprint(world)
+        return [], [world]
+    for key in world.keys:
+        if key not in world.chestdic:
             continue
+        wchests = world.chestdic[key]
         for i in xrange(0, len(wchests)):
             depprint('use {} for ({}/{}) {}'.format(key, i, len(wchests), wchests[i]))
             clone = world.clone()
@@ -128,14 +142,18 @@ def solve_(world, depth):
             clone.keys.remove(key)
             clone.allkeys[key] -= 1
             clone.keys += chest.keys
+            clone.keys.sort()
             del(chests[i])
             if len(clone.chestdic[key]) == 0:
                 del(clone.chestdic[key])
-            result = solve_(clone, list(depth) + [chest.num])
+            result, worlds = solve_(clone, list(depth) + [chest.num])
             if isinstance(result, list):
-                return [chest.num] + result
+                nodes = [chest.num] + result
+                okprint(nodes)
+                okprint(world)
+                return nodes, [world] + worlds
     depprint('==> FAIL')
-    return False
+    return False, []
 
 
 if __name__ == '__main__':
@@ -145,7 +163,6 @@ if __name__ == '__main__':
         keycount, chestcount = map(int, lines[0].split(' '))
         lines += [raw_input() for x in xrange(0, chestcount)]
         world = World(lines)
-        world.cleanup()
         #print '=================================='
         result = solve(world)
         print 'Case #%d:' % xx,
